aaf6869739eff5ebf5a6c4b999e1a5d4bd9fadd5
Daniela Soares <danielasmelo232@gmail.com>
2024-03-14 18:08:00 -0300

    primeiro commit

---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+//aula335||338
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Daniela Soares
+        </span>
+    </footer>
